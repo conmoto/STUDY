d97305e3680fa65234d7ff27aa18f867f229394f
conmoto <conmoto33@naver.com>
2021-02-16 18:18:10 +0900

    C# OPP

---
+++ C#/CSharp/OOP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    //객체 (OOP Object Oriented Programming)
+    class OOP
+    {
+        class Knight
+        {
+            public int hp;
+            public int attack;
+
+            public void Move()
+            {
+                Console.WriteLine("Knight Move");
+            }
+            public void Attack()
+            {
+                Console.WriteLine("Knight Attack");
+            }
+        }
+
+
+        static void Run()
+        {
+            Knight knight = new Knight();
+
+            knight.hp = 100;
+            knight.attack = 10;
+        }
+    }
+}
