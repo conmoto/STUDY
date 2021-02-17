6688612a071f95c211863ebebfdf05537cbd292d
conmoto <conmoto33@naver.com>
2021-02-17 14:26:33 +0900

    C# string

---
+++ C#/CSharp/CSharpString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class CSharpString
+    {
+        public static void Run()
+        {
+            string name = "Harry Potter";
+
+            //찾기
+            bool found = name.Contains("Harry");
+            int index = name.IndexOf("P");
+
+            //변형
+            name += "Junior";
+            string lowercaseString = name.ToLower();
+            string uppercaseString = name.ToUpper();
+            string newName = name.Replace('P', 'C');
+
+            //분할
+            string[] names = name.Split(new char[] {' '});
+            string substringName = name.Substring(6);
+        }
+    }
+}
